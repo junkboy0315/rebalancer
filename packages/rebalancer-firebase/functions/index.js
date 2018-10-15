352e3e8e31eb5d7462797ffca98675129627817f
stam <junkboy0315@gmail.com>
2018-10-15 11:49:10 +0900

    add firebase function to delete anonymous users

--- packages/rebalancer-firebase/functions/index.js
+++ packages/rebalancer-firebase/functions/index.js
@@ -19,3 +19,41 @@ exports.cleanupUserData = functions.auth.user().onDelete(user => {
     .then(console.log('cleanup succeed'))
     .catch(err => console.log('cleanup  (partially) failed', err));
 });
+
+exports.cleanupAnonymousAccounts = functions.https.onRequest((req, res) => {
+  if (req.method !== 'POST') return res.sendStatus(404);
+
+  function deleteAnonymousUsers(nextPageToken) {
+    firebaseAdmin
+      .auth()
+      .listUsers(500, nextPageToken)
+      .then(listUsersResult => {
+        console.log('listUsersResult:', listUsersResult);
+        listUsersResult.users.forEach(function(userRecord) {
+          if (userRecord.providerData.length === 0) {
+            //this user is anonymous
+            console.log('deleting:', userRecord); // do your delete here
+            firebaseAdmin
+              .auth()
+              .deleteUser(userRecord.uid)
+              .then(() => {
+                console.log('Successfully deleted user');
+              })
+              .catch(error => {
+                console.log('Error deleting user:', error);
+              });
+          }
+        });
+        console.log('pageToken:', listUsersResult.pageToken);
+        if (listUsersResult.pageToken) {
+          deleteAnonymousUsers(listUsersResult.pageToken);
+        }
+      })
+      .catch(function(error) {
+        console.log('Error listing users:', error);
+      });
+  }
+
+  deleteAnonymousUsers();
+  res.sendStatus(200);
+});
