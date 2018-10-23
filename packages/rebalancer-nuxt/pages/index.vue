4728eec329c097304296a49cfb297d215026f386
stam <junkboy0315@gmail.com>
2018-10-24 08:19:48 +0900

    add shadow to button & notification

--- packages/rebalancer-nuxt/pages/index.vue
+++ packages/rebalancer-nuxt/pages/index.vue
@@ -65,8 +65,6 @@ export default {
   }
 
   .button {
-    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-      0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
     font-size: 1.25rem;
     height: 54px;
     width: 280px;
